4833ab8fd110d4022e5e77839fcd342030eb3315
Amir <amirzny@gmail.com>
2022-05-12 20:34:09 +0300

    -

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'main.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
